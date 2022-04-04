b501a3424939a0b4bfb6a6c51b85538396b06d13
BinalRamani29 <102648172+BinalRamani29@users.noreply.github.com>
2022-04-04 14:42:48 +0530

    Edit profile Get API call Done

--- app/src/main/java/com/furniture/ui/editpersonalinfo/EditPersonalInfo.kt
+++ app/src/main/java/com/furniture/ui/editpersonalinfo/EditPersonalInfo.kt
@@ -1,34 +1,90 @@
 package com.furniture.ui.editpersonalinfo
 
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
 import com.furniture.R
+import com.furniture.data.model.admin1.Admin1
 import com.furniture.databinding.EditPersonalInfoFragmentBinding
+import com.furniture.interfaces.AdminDetailInterface
+import com.furniture.interfaces.CommonInterface
+import com.furniture.utlis.dialogs.ProgressDialog
+import com.furniture.utlis.uid
 import dagger.android.support.DaggerFragment
+import java.net.URL
 import javax.inject.Inject
 
-class EditPersonalInfo : DaggerFragment(R.layout.edit_personal_info_fragment) {
+
+class EditPersonalInfo : DaggerFragment(R.layout.edit_personal_info_fragment), AdminDetailInterface,
+    CommonInterface {
     private lateinit var binding: EditPersonalInfoFragmentBinding
 
-    private val viewModels by viewModels<EditPersonalInfoVM>()
 
     @Inject
     lateinit var viewModel: EditPersonalInfoVM
 
+    private lateinit var progressDialog: ProgressDialog
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         binding = EditPersonalInfoFragmentBinding.inflate(inflater, container, false)
-        binding.vm = viewModels
+//        binding.vm = viewModels
         return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(EditPersonalInfoVM::class.java)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
     }
+
+    private fun init() {
+        progressDialog = ProgressDialog(requireActivity())
+        viewModel.adminDetailInterface = this
+        viewModel.commonInterface = this
+        binding.viewModel = viewModel
+        viewModel.getAdminById(uid)
+    }
+
+    override fun onFailure(message: String) {
+        progressDialog.dismiss()
+    }
+
+    override fun onFailureAPI(message: String) {
+        progressDialog.dismiss()
+    }
+
+    override fun onStarted() {
+        progressDialog.show()
+    }
+
+
+    override fun onAdminDetails(admin1: Admin1) {
+        Glide.with(requireContext()).load(admin1.profilePhoto).placeholder(R.drawable.ic_user)
+            .error(R.drawable.ic_user).into(binding.imgDefault)
+        if (admin1.coverPhoto != null) {
+            val url = URL(admin1.coverPhoto)
+            val bitmap = BitmapFactory.decodeStream(url.openConnection().getInputStream())
+            val image: Drawable = BitmapDrawable(requireContext().resources, bitmap)
+            binding.layoutGrandiant.background = image
+        } else {
+            binding.layoutGrandiant.background =
+                ContextCompat.getDrawable(requireContext(), R.drawable.bg_gradient_profile)
+        }
+
+        progressDialog.dismiss()
+    }
+//    override fun onActivityCreated(savedInstanceState: Bundle?) {
+//        super.onActivityCreated(savedInstanceState)
+//        viewModel = ViewModelProvider(this).get(EditPersonalInfoVM::class.java)
+//    }
 
 }
--- app/src/main/java/com/furniture/ui/editpersonalinfo/EditPersonalInfoVM.kt
+++ app/src/main/java/com/furniture/ui/editpersonalinfo/EditPersonalInfoVM.kt
@@ -1,12 +1,26 @@
 package com.furniture.ui.editpersonalinfo
 
 import android.view.View
+import androidx.databinding.BindingAdapter
+import androidx.databinding.ObservableField
 import androidx.lifecycle.ViewModel
 import androidx.navigation.findNavController
+import com.bumptech.glide.Glide
 import com.furniture.R
+import com.furniture.data.apis.WebService
+import com.furniture.data.model.admin1.Admin1
+import com.furniture.data.network.Coroutines
+import com.furniture.interfaces.AdminDetailInterface
+import com.furniture.interfaces.CommonInterface
+import com.furniture.utlis.AUTHORIZATION
+import com.google.android.gms.common.api.ApiException
+import com.makeramen.roundedimageview.RoundedImageView
 import javax.inject.Inject
 
-class EditPersonalInfoVM @Inject constructor() : ViewModel() {
+class EditPersonalInfoVM @Inject constructor(private val webService: WebService) : ViewModel() {
+    var commonInterface: CommonInterface? = null
+    var adminDetailInterface: AdminDetailInterface? = null
+    var userField = ObservableField<Admin1>()
 
     fun onClick(view: View) {
         when (view.id) {
@@ -14,4 +28,31 @@ class EditPersonalInfoVM @Inject constructor() : ViewModel() {
         }
     }
 
+    fun getAdminById(string: String) {
+        commonInterface!!.onStarted()
+        Coroutines.main {
+            try {
+                val authResponse = webService.getAdminByID(string, AUTHORIZATION)
+                if (authResponse.isSuccessful) {
+                    authResponse.body().let {
+                        adminDetailInterface?.onAdminDetails(authResponse.body()!!.payload.admin[0])
+                        userField.set(authResponse.body()!!.payload.admin[0])
+                    }
+                } else {
+                    commonInterface!!.onFailureAPI(authResponse.message())
+                }
+            } catch (e: ApiException) {
+                e.message?.let { commonInterface!!.onFailure(it) }
+            } catch (ex: Exception) {
+                ex.message?.let { commonInterface!!.onFailure(it) }
+            }
+        }
+    }
+
+    @BindingAdapter("imageUrl")
+    fun loadImage(view: RoundedImageView, url: Admin1?) {
+        if (url != null) {
+            Glide.with(view.context).load(url.profilePhoto).into(view)
+        }
+    }
 }

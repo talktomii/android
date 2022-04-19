ebed6c0ff5c514c31d231ab5ff0feae21f8d7991
Aamir <aamir.bashir@codebrewinnovations.com>
2022-04-19 05:51:55 +0530

    test

--- app/src/main/java/com/talktomii/ui/loginSignUp/splash/SplashFragment.kt
+++ app/src/main/java/com/talktomii/ui/loginSignUp/splash/SplashFragment.kt
@@ -1,9 +1,13 @@
 package com.talktomii.ui.loginSignUp.splash
 
 import android.content.Intent
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Base64
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,6 +17,8 @@ import com.talktomii.R
 import com.talktomii.utlis.PrefsManager
 import com.talktomii.utlis.getUser
 import dagger.android.support.DaggerFragment
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
 import javax.inject.Inject
 
 
@@ -35,7 +41,7 @@ class SplashFragment : DaggerFragment() {
         Handler(Looper.getMainLooper()).postDelayed({
             openFragments()
         }, 4000)
-
+//        getHashkey()
     }
 
     private fun openFragments() {
@@ -56,4 +62,22 @@ class SplashFragment : DaggerFragment() {
 
 
     }
+//
+//    fun getHashkey() {
+//        try {
+//            val info: PackageInfo = requireActivity().packageManager.getPackageInfo(
+//                requireActivity().applicationContext.packageName,
+//                PackageManager.GET_SIGNATURES
+//            )
+//            for (signature in info.signatures) {
+//                val md: MessageDigest = MessageDigest.getInstance("SHA")
+//                md.update(signature.toByteArray())
+//                Log.e("Base64:: ", Base64.encodeToString(md.digest(), Base64.NO_WRAP))
+//            }
+//        } catch (e: PackageManager.NameNotFoundException) {
+//            //  Log.d("Name not found", e.getMessage(), e)
+//        } catch (e: NoSuchAlgorithmException) {
+//            // Log.d("Error", e.getMessage(), e)
+//        }
+//    }
 }
